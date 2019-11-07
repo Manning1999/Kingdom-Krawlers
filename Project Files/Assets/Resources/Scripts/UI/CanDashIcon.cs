77230135c04ec3565e9eb105068d58d8a35f1d48
AdenKlitscher <s192013@students.aie.edu.au>
2019-11-07 21:32:28 +1030

    Started On Player Hud and Pause Menu
    Added Aden folder
    Added UI test scene
    Started UI

---
+++ Project Files/Assets/Resources/Scripts/UI/CanDashIcon.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanDashIcon : MonoBehaviour
+{
+    //public GameObject Player;
+    public Material statusMaterial;
+    
+    private bool canDash;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        // Player.GetComponent<PlayerController>();
+        if (Input.GetButtonDown("Fire2"))
+        {
+            canDash = true;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            canDash = false;
+        }
+
+
+        if (canDash == true)
+        {
+            
+            GetComponent<Image>().color = Color.green;
+        }
+
+        if (canDash == false)
+        {
+            
+            GetComponent<Image>().color = Color.red;
+        }
+
+        
+        
+
+    }
+}
