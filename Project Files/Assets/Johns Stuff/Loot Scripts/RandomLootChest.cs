aad0eade5a5bf94760e4793d2a1e006d0c9b3454
jmcguirk3 <50133567+jmcguirk3@users.noreply.github.com>
2019-11-12 21:06:43 +1100

    Added loot scripts

---
+++ Project Files/Assets/Johns Stuff/Loot Scripts/RandomLootChest.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomLootChest : MonoBehaviour
+{
+    public List<GameObject> lights;
+    public float[] table =
+    {
+     9, // Coins (5-500)
+     9, // Arrows
+     9, // Grey Sword
+     9, // Health Potion      
+     9, // Grey Bow
+     9, // Blue Sword
+     9, // Blue Bow
+     9, // Health Regen Potion
+     9, // Speed Potion
+     9, // Strength Potion
+     9, // Invisibility Potion
+     9, // Haste Potion
+    };
+
+    public float total;
+    public float randomNumber;
+    private void Start()
+    {
+        // tally total weight
+        // draw random number between 0 and total weight (100) 
+
+        foreach (var item in table)
+        {
+            total += item;
+        }
+
+        randomNumber = UnityEngine.Random.Range(0, (total));
+
+        for (int i = 0; i < table.Length; i++)
+        {
+            if (randomNumber <= table[i])
+            {
+                lights[i].SetActive(true);
+                return;
+            }
+            else
+            {
+                randomNumber -= table[i];
+
+            }
+        }
+
+
+        //randomnumber = 49
+
+        // is 40 <= 65
+        // Coins (5 - 15)
+
+        //randomnumber = 66
+        // is 66 <= 65
+        //no
+        // 66 - 65 = 1
+        //1 <= 15
+        //Arrows
+
+        //randomnumber = 95
+        //is 95 <= 65
+        // 95 - 65 = 30
+        // 30 <= 15?
+        // 30 - 15 = 15
+        // 15 <= 10?
+        // 15 - 10 = 5
+        // 5 <= 5?
+        // Health potion
+    }
+}
