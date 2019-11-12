8ffafb8eb2ae35b5005c2620b946f19dd3e16a6d
JSimpss <jacksimpkin@hotmail.com>
2019-11-12 20:30:59 +1030

    Added Fireball Script
    fireball script for skeleton, yet to attach

---
+++ Project Files/Assets/Resources/Scripts/Other/Fireball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float speed = 8f;
+    public float lifeDuration = 2f;
+    public int damage = 15;
+
+    private float lifeTimer;
+
+
+    private void OnEnable()
+    {
+        lifeTimer = lifeDuration;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        // make the projectile move
+        transform.position += transform.forward * Time.deltaTime;
+
+        // check if the bullet should be destroyed
+        lifeTimer -= Time.deltaTime;
+        if (lifeTimer <= 0f)
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+}
