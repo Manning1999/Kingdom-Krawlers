bee393d38399ab3c51c53a96d4fc1657979d4c87
AdenKlitscher <s192013@students.aie.edu.au>
2019-11-19 21:09:21 +1030

    Programed the mainmenu and started the inventory code

---
+++ Project Files/Assets/Resources/Scripts/UI/AudioButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioButton : MonoBehaviour
+{
+    public GameObject mainMenuUI;
+    public GameObject audioUI;
+
+    public void ButtonClick()
+    {
+        mainMenuUI.SetActive(false);
+        audioUI.SetActive(true);
+    }
+}
---
+++ Project Files/Assets/Resources/Scripts/UI/VideoButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoButton : MonoBehaviour
+{
+    public GameObject mainMenuUI;
+    public GameObject videoUI;
+
+    public void ButtonClick()
+    {
+        mainMenuUI.SetActive(false);
+        videoUI.SetActive(true);
+    }
+}
