8ca1921559d955923ff0fd53768482a17850482f
AdenKlitscher <s192013@students.aie.edu.au>
2019-11-26 16:36:15 +1030

    Coded Main Menu and worked on hud which follows the mouse

---
+++ Project Files/Assets/Resources/Scripts/UI/ResumeButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResumeButton : MonoBehaviour
+{
+    public GameObject mainMenuUI;
+    public GameObject videoUI;
+    public GameObject audioUI;
+
+    public void ButtonClick()
+    {
+        mainMenuUI.SetActive(true);
+        videoUI.SetActive(false);
+        audioUI.SetActive(false);
+    }
+}
