efab8d6c1a477b8fbd12ab0757f9c625de5ac7e8
AdenKlitscher <s192013@students.aie.edu.au>
2019-12-02 20:30:03 +1030

    Made the Pause menu activatable by pressing escape

---
+++ Project Files/Assets/Resources/Scripts/UI/PauseTransition.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseTransition : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject hud;
+    public bool escapePress = false;
+
+
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            escapePress = !escapePress;
+        }
+
+        if (escapePress == false)
+        {
+            hud.SetActive(true);
+            pauseMenu.SetActive(false);
+        }
+
+        if (escapePress == true)
+        {
+            hud.SetActive(false);
+            pauseMenu.SetActive(true);
+        }
+    }
+}
