a562da5e6a491fcdba7cf66416394e2741ea97fb
AdenKlitscher <s192013@students.aie.edu.au>
2019-12-05 18:25:13 +1030

    Fixed some bugs with ui

--- Project Files/Assets/Resources/Scripts/UI/goToMainmenu.cs
+++ Project Files/Assets/Resources/Scripts/UI/goToMainmenu.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class goToMainmenu : MonoBehaviour
 {
+    public GameObject pauseMenu;
+
     public void ButtonClick()
     {
+        pauseMenu.SetActive(false);
         SceneManager.LoadScene("Main Menu");
     }
 }
