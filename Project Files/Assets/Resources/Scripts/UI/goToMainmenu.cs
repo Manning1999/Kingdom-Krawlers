3a4a4a97dddbc91ba2096ca1c75640568d89daf8
AdenKlitscher <s192013@students.aie.edu.au>
2019-12-05 16:18:21 +1030

    Added some functionality to the pause menu

---
+++ Project Files/Assets/Resources/Scripts/UI/goToMainmenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class goToMainmenu : MonoBehaviour
+{
+    public void ButtonClick()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
