693f019d848ff00eda6d8865c54eedfd469d3bfd
AdenKlitscher <s192013@students.aie.edu.au>
2019-12-17 14:32:41 +1030

    Updated Credit

---
+++ Project Files/Assets/Resources/Scripts/UI/HealthPotionHud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthPotionHud : MonoBehaviour
+{
+    public int HealthPotionCount;
+    public Text HealthPotionCountText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HealthPotionCountText.text = HealthPotionCount.ToString();
+    }
+}
